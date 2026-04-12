843f06350ce53dd45fbad56d360759d9d7e791ad
PatriceL <plindivat@free.fr>
2026-04-12 03:03:02 +0200

    reorder AppHeader toolbar components: move QuickNav before NetGrossToggle, remove ml-4 margin from DetailedNoteToggle, and change QuickNav visibility breakpoint from sm to lg

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -193,10 +193,10 @@
             </div>
             <div v-if="userStore.user && currentDatabase"
                 class="w-full flex items-center justify-between border-t border-gray-200 dark:border-gray-700 bg-gray-50/50 dark:bg-gray-800/50">
-                <div class="container mx-auto py-4 px-4 lg:flex flex-row">
+                <div class="container mx-auto py-4 px-4 flex items-center gap-4">
+                    <QuickNav class="hidden lg:block" v-if="userStore.user.settings_object?.showQuickNav" />
                     <NetGrossToggle v-model="userStore.displayModeNet" />
-                    <DetailedNoteToggle class="ml-4" />
-                    <QuickNav class="hidden sm:block" v-if="userStore.user.settings_object?.showQuickNav" />
+                    <DetailedNoteToggle />
                 </div>
             </div>
             <div class="container mx-auto flex justify-between items-center px-4">
